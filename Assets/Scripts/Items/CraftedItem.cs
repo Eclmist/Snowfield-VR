cc26d5c9e95e02c38eb258bca4bae1d843f64ae0
Unknown <marcusheadgotscarm16@gmail.com>
2017-04-10 16:50:58 +0800

    Big commit
    added base item hierachy
    implemented blacksmith item heating and cooling
    added OrderBoard class
    created order board prefab
    created request prefab
    changed heating to be based on distance from center

---
+++ Assets/Scripts/Items/CraftedItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftedItem : GenericItem {
+
+    protected virtual void UseItem()
+    {
+        Debug.Log("You are using " + this.name);
+    }
+
+    void Start()
+    {
+
+    }
+
+
+	
+}
