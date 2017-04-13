a759fe200dabb940559552170f9cf53016038a05
Chen Xiang <xMashedxTomatox@gmail.com>
2017-04-13 21:54:35 +0800

    Ready for test throwingi and bug fixes of joints

--- Assets/VR_Controller_Custom.cs
+++ Assets/VR_Controller_Custom.cs
@@ -14,7 +14,7 @@ public class VR_Controller_Custom : MonoBehaviour
 
     [SerializeField] private Controller_Handle handle;
     [SerializeField] private LayerMask interactableLayer;
-    [SerializeField] private Rigidbody attachedPoint;
+    private Rigidbody attachedPoint;
     private FixedJoint attachedJoint = null;
     private SteamVR_TrackedObject trackedObject;
     private GameObject interactableObject;
@@ -23,6 +23,7 @@ public class VR_Controller_Custom : MonoBehaviour
     void Awake()
     {
         trackedObject = GetComponent<SteamVR_TrackedObject>();
+        attachedPoint = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -34,10 +35,11 @@ public class VR_Controller_Custom : MonoBehaviour
     private void ControllerInput()
     {
         SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObject.index);
-        if (interactableObject != null && (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)))
+        if (attachedJoint == null && interactableObject != null && (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)))
         {
             IInteractable interactable = interactableObject.GetComponent<IInteractable>();
             interactable.Interact(transform);
+            DestroyImmediate(interactableObject.GetComponent<FixedJoint>());
             attachedJoint = interactableObject.AddComponent<FixedJoint>();
             attachedJoint.connectedBody = attachedPoint;
 
@@ -57,8 +59,10 @@ public class VR_Controller_Custom : MonoBehaviour
         else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
         {
             IInteractable interactable = null;
+            Rigidbody rigidBody = attachedJoint.GetComponent<Rigidbody>();
             DestroyImmediate(attachedJoint);
             attachedJoint = null;
+            
             switch (handle)
             {
                 case Controller_Handle.LEFT:
@@ -75,6 +79,20 @@ public class VR_Controller_Custom : MonoBehaviour
             {
                 interactable.StopInteraction(transform);
             }
+
+            Transform origin = trackedObject.origin ? trackedObject.origin : trackedObject.transform.parent;
+            if (origin != null)
+            {
+                rigidBody.velocity = origin.TransformVector(device.velocity);
+                rigidBody.angularVelocity = origin.TransformVector(device.angularVelocity);
+            }
+            else
+            {
+                rigidBody.velocity = device.velocity;
+                rigidBody.angularVelocity = device.angularVelocity;
+            }
+
+            rigidBody.maxAngularVelocity = rigidBody.angularVelocity.magnitude;
         }
     }
 
