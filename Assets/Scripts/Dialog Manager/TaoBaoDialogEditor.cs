f8663b340b10a9deb92bcabf18d5a9f04ea88103
jkbhk <marcusheadgotscarm16@gmail.com>
2017-04-14 18:40:42 +0800

    Changed DialogManager to read audio file names

--- Assets/Scripts/Dialog Manager/TaoBaoDialogEditor.cs
+++ Assets/Scripts/Dialog Manager/TaoBaoDialogEditor.cs
@@ -1,23 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [ExecuteInEditMode]
-public class TaoBaoDialogEditor : MonoBehaviour {
+public class TaoBaoDialogEditor : MonoBehaviour
+{
 
     [System.Serializable]
-    public struct Session
+    public class Session
     {
         [Space(20f)]
-        [Tooltip("Title of the conversation")]   public string title;
-        [Tooltip("List of lines to say")] public List<Line> lines;
+        [Tooltip("Title of the conversation")]
+        public string title;
+        [Tooltip("List of lines to say")]
+        public List<Line> lines;
     }
 
     [System.Serializable]
-    public struct Line
+    public class Line
     {
-        [Tooltip("A single line of message")] [TextArea] public string message;
-        [Tooltip("Whatever sound that is going to play in this line")] public AudioClip clip;
+        [Tooltip("A single line of message")]
+        [TextArea]
+        public string message;
+        [NonSerialized]
+        public AudioClip clip;
+        [Tooltip("Name of audio that is going to play in this line")]
+        public string audioName;
+
+
     }
 
 
@@ -29,18 +41,56 @@ public class TaoBaoDialogEditor : MonoBehaviour {
     private void Awake()
     {
         sessions = SerializeManager.Instance.Load("Dialogs") as List<Session>;
+        LoadAudioFiles();
+
     }
 
     private void Update()
     {
         if (save = !save)
         {
-            SerializeManager.Instance.Save("Dialogs",sessions);
+            SerializeManager.Instance.Save("Dialogs", sessions);
             Debug.Log("Latest \"" + sessions[sessions.Count - 1].title + "\" saved");
         }
-            
+
     }
 
+    private void LoadAudioFiles()
+    {
+        foreach (Session s in sessions)
+        {
+            foreach (Line l in s.lines)
+            {
+                if (l.audioName != "")
+                {
+                    l.clip = Resources.Load(ConvertToPath(l.audioName)) as AudioClip;
+                }
+
+
+            }
+        }
+    }
+
+
+    private string ConvertToPath(string s)
+    {
+        return "Dialog expressions/" + s + ".wav";
+    }
+
+
+
+    //private void ConvertToPath()
+    //{
+    //    foreach(Session s in sessions)
+    //    {
+    //        foreach(Line l in s.lines)
+    //        {
+    //            if(l.clip != null)
+    //            l.path = UnityEditor.AssetDatabase.GetAssetPath(l.clip);
+
+    //        }
+    //    }
+    //}
 
 
 
