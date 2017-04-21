5beaa02f6c324915181b9a3d4e5ef2c602656e90
Unknown <ivan_leong_97@hotmail.com>
2017-04-21 14:54:17 +0800

    Update ItemsManager

--- Assets/Scripts/Manager/ItemsManager.cs
+++ Assets/Scripts/Manager/ItemsManager.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Xml.Serialization;
 
+using System.Reflection;
+
+//public enum PotionType
+//{
+//Red,
+//Blue
+//}
+
 public class ItemsManager
 {
     [XmlArray("BlacksmithItems")]
@@ -12,4 +20,33 @@ public class ItemsManager
     [XmlArray("AlchemyItems")]
     [XmlArrayItem("AlchemyItem")]
     public static List<AlchemyItem> AlchemyItemList;
+
+    [XmlArray("CraftedItems")]
+    [XmlArrayItem("CraftedItem")]
+    public static List<CraftedItem> CraftedItemList;
+
+    //public Dictionary<, System.Type> 
+
+    //public static void asdsa()
+    //{
+    //    var assembly = Assembly.GetExecutingAssembly();
+    //    var types = assembly.GetTypes();
+
+    //    foreach (var t in types)
+    //    {
+    //        if (t.GetType() == typeof(Potion))
+    //        {
+    //            var fields = t.GetFields();
+
+    //            foreach (var f in fields)
+    //            {
+    //                if (f.ReflectedType == typeof(PotionType))
+    //                {
+    //                    System.Type 
+    //                }
+    //            }
+    //        }
+    //    }
+
+    //}
 }
