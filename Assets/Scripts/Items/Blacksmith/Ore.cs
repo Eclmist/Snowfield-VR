d743da9c2b2d66dcb36b5c9c52219bf381e2d1d0
Unknown <marcusheadgotscarm16@gmail.com>
2017-04-24 17:12:58 +0800

    Implemented Ore class

--- Assets/Scripts/Items/Blacksmith/Ore.cs
+++ Assets/Scripts/Items/Blacksmith/Ore.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ore : BlacksmithItem{
+public class Ore : GenericItem{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private PhysicalMaterial physicalMaterial;
+
+    public override void UpdatePosition()
+    {
+        throw new NotImplementedException();
+    }
 }
