c941daa1f20067cef71ff96c6727b498c47e13d4
Unknown <marcusheadgotscarm16@gmail.com>
2017-04-24 17:12:36 +0800

    Created OrderTemplate and PhysicalMaterial class

---
+++ Assets/Scripts/Items/Blacksmith/OrderTemplate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class OrderTemplate : MonoBehaviour {
+
+    public Sprite sprite;
+    [HideInInspector]public string spritePath;
+    public int spriteIndex;
+    public Mesh mesh;
+    public int baseGold;
+    public int levelUnlocked;
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/Items/Blacksmith/PhysicalMaterial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PhysicalMaterial : MonoBehaviour {
+
+    [SerializeField]private int costMultiplier;
+    [SerializeField]private string m_name;
+    [SerializeField]
+    private enum TYPE
+    {
+        IRON,
+        COPPER,
+        STEEL
+    };
+    
+
+
+
+}
