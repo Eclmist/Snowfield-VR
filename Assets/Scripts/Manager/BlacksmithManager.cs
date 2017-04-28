f66c80dc4b69849bbaf524171032ece101ced71f
Unknown <marcusheadgotscarm16@gmail.com>
2017-04-28 16:53:48 +0800

    Added Blacksmith Manager

---
+++ Assets/Scripts/Manager/BlacksmithManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlacksmithManager : MonoBehaviour {
+
+    public static BlacksmithManager Instance;
+
+    private List<Ingot> availableIngots;
+
+
+	void Awake()
+    {
+        Instance = this;
+        availableIngots = new List<Ingot>();
+
+        StoreReferences();
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void StoreReferences()
+    {
+        Object[] temp = Resources.LoadAll("Ingots");
+
+        foreach (Object o in temp)
+        {
+            availableIngots.Add(o as Ingot);
+        }
+    }
+
+   
+
+   
+}
