fe66de230b3c984112e3fdc18541a0e0168e8bb4
Unknown <ivan_leong_97@hotmail.com>
2017-05-11 14:43:58 +0800

    Added Horse

---
+++ Assets/Horse/Horse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Horse : MonoBehaviour {
+
+    [SerializeField]
+    protected float speed;
+
+	void Update () {
+        transform.position += Vector3.forward * Time.deltaTime * speed;
+	}
+}
