c45a4def29946c95c720b14e644177aa41def171
Unknown <marcusheadgotscarm16@gmail.com>
2017-05-24 16:18:07 +0800

    Added IDimensionable

---
+++ Assets/Scripts/Interface/IDimensionable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDimensionable  {
+
+    Sprite Icon { get; set; }
+    string Name { get; set; }
+    GameObject objReference { get;}
+
+
+}
