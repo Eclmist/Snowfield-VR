ae42674ef8255fab4a0e449bd53a0dd365175daa
Eclmist <Eclmist@yahoo.com>
2017-05-28 19:58:27 +0800

    Finished dual player UI
    Added multiple UI sound FX

--- Assets/Scripts/UI/MultiActorUI.cs
+++ Assets/Scripts/UI/MultiActorUI.cs
@@ -4,18 +4,69 @@ using UnityEngine;
 
 public class MultiActorUI : MonoBehaviour
 {
-	private GameObject initiator;
-	private GameObject receiver;
+	[SerializeField] private GameObject initiator;
+	[SerializeField] private GameObject receiver;
 
+	[SerializeField] private float distanceFromActor = 0.3F;
+	[SerializeField] private float initiatorHeight = 1.5F;
+	[SerializeField] private float receiverHeight = 1.5F;
 
+	[SerializeField] private float rotationSpeed = 10;
 
-	// Use this for initialization
-	void Start () {
-		
+	private Animator anim;
+
+	protected void Start()
+	{
+		// Set starting position
+		Vector3 targetPosition = initiator.transform.position +
+			(receiver.transform.position- initiator.transform.position) * distanceFromActor;
+
+		targetPosition.y = initiatorHeight;
+		transform.position = targetPosition;
+
+		// Set starting rotation
+		Vector3 targetLookAt = initiator.transform.position;
+		targetLookAt.y = transform.position.y;
+		transform.LookAt(targetLookAt);
+
+		// Get Anim
+		anim = GetComponent<Animator>();
+		if (anim == null)
+			Destroy(this);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+
+	public void RotateToTarget()
+	{
+		StartCoroutine(Rotate());
+	}
+
+	private IEnumerator Rotate()
+	{
+		// Save starting transform state
+		Vector3 startPos = transform.position;
+		Quaternion startRot = transform.rotation;
+
+		// Calculate target point (rotation about pivot)
+		Vector3 pivot = (initiator.transform.position + receiver.transform.position) / 2;
+		pivot.y = startPos.y;
+
+		// Calculate target local rotation
+		Quaternion targetRot = 
+			Quaternion.LookRotation(receiver.transform.position- initiator.transform.position, Vector3.up);
+
+		for (float t = 0; t < 1; t += rotationSpeed)
+		{
+			Vector3 direction = startPos - pivot; // get point direction relative to pivot
+			direction = Quaternion.Euler(new Vector3(0, t/1 * 180, 0)) * direction; // generate Rotation
+			Vector3 targetPoint = direction + pivot; // calculate rotated point
+			transform.position = targetPoint;
+
+			transform.rotation = Quaternion.Slerp(startRot, targetRot, t);
+			yield return new WaitForFixedUpdate();
+		}
+
+		anim.SetTrigger("Open");
+		Destroy(this);
 	}
 }
