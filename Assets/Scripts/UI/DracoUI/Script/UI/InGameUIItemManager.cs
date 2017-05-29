0035c4f6c9c94f88d2d27cc2534c0c1c9fc11758
Unknown <dragolee@hotmail.com>
2017-05-29 18:09:26 +0800

    UI files migration

---
+++ Assets/Scripts/UI/DracoUI/Script/UI/InGameUIItemManager.cs
@@ -0,0 +1,59 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUIItemManager : MonoBehaviour {
+    public static InGameUIItemManager Instance;
+    private Transform[] options, temp;
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Overlord.");
+            Destroy(this);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        options = new Transform[3];
+        int i = 0;
+        temp = GetComponentsInChildren<Transform>(true);
+
+        foreach (Transform t in temp)
+        {
+            if (t.tag.Equals("InGameMenuItem"))
+            {
+                options[i] = t;
+                Debug.Log(options[i]);
+                i++; 
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject GetSpecificOption(string n)
+    {
+        Transform temp = null;
+        foreach (Transform t in options)
+        {
+            if (t.name == n)
+            {
+                temp = t;
+                
+            }
+        }
+        return temp.gameObject;
+    }
+}
---
+++ Assets/Scripts/UI/DracoUI/Script/UI/MainMenu.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum MainMenuState
+{
+    IDLE,
+    CONTINUE,
+    NEW_GAME,
+    SETTINGS,
+    CREDITS,
+    QUIT
+}
+
+
+public class MainMenu : MonoBehaviour
+{
+    public static MainMenu Instance;
+    protected MainMenuState curState = MainMenuState.NEW_GAME, 
+        lastState = MainMenuState.IDLE, 
+        lastLastState = MainMenuState.IDLE, 
+        nextState = MainMenuState.SETTINGS;
+
+    private GameObject settings;
+
+    void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Overlord.");
+            Destroy(this);
+        }
+    }
+
+   
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //test
+        if(settings == null)
+            settings = MainMenuItemManager.Instance.GetSpecificChild("BGM"); 
+        
+        //Main menu state machine
+        if (curState == MainMenuState.IDLE)
+        {
+
+        }
+        else if (curState == MainMenuState.CONTINUE)
+        {
+            SettingsSet();
+            Text txt = GetComponentInChildren<Text>();
+            TextChange(txt, "Continue");
+            lastState = curState;
+            curState = MainMenuState.IDLE;
+        }
+        else if (curState == MainMenuState.NEW_GAME)
+        {
+            SettingsSet();
+            Text txt = GetComponentInChildren<Text>();
+            TextChange(txt, "New Game");
+            lastLastState = MainMenuState.IDLE;
+            lastState = curState;
+            nextState = MainMenuState.SETTINGS;
+            curState = MainMenuState.IDLE;
+        }
+        else if (curState == MainMenuState.SETTINGS)
+        {
+            SettingsSet();
+            MainMenuItemManager.Instance.GetChild();
+            Text txt = GetComponentInChildren<Text>();
+            TextChange(txt, "Settings");
+            lastLastState = MainMenuState.NEW_GAME;
+            lastState = curState;
+            nextState = MainMenuState.CREDITS;
+            curState = MainMenuState.IDLE;
+        }
+        else if (curState == MainMenuState.CREDITS)
+        {
+            SettingsSet();
+            Text txt = GetComponentInChildren<Text>();
+            TextChange(txt, "Credits");
+            lastLastState = MainMenuState.SETTINGS;
+            lastState = curState;
+            nextState = MainMenuState.QUIT;
+            curState = MainMenuState.IDLE;
+            
+        }
+        else if (curState == MainMenuState.QUIT)
+        {
+            SettingsSet();
+            Text txt = GetComponentInChildren<Text>();
+            TextChange(txt, "Quit");
+            lastLastState = MainMenuState.CREDITS;
+            lastState = curState;
+            nextState = MainMenuState.IDLE;
+            curState = MainMenuState.IDLE;
+        }
+
+    }
+
+    private void TextChange(Text txt, string newTxt)
+    {
+        txt.text = newTxt;
+    }
+
+    
+
+    public void SetState(MainMenuState state)
+    {
+        curState = state;
+    }
+
+    public MainMenuState GetLLState()
+    {
+        return lastLastState;
+    }
+
+    public MainMenuState GetLastState()
+    {
+        return lastState;
+    }
+
+    public MainMenuState GetNextState()
+    {
+        return nextState;
+    }
+   
+
+    private void SettingsSet()
+    {
+        if (settings != null && curState == MainMenuState.SETTINGS)
+        {
+            settings.SetActive(true);
+        }
+        else if (settings != null && curState != MainMenuState.SETTINGS)
+        {
+            settings.SetActive(false);
+        }
+
+    }
+}
