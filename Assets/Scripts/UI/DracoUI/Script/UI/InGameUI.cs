2a42f03412c77cf8f2b6c9a78495953d87549a84
Unknown <dragolee@hotmail.com>
2017-05-29 18:09:26 +0800

    UI files migration

---
+++ Assets/Scripts/UI/DracoUI/Script/UI/InGameUI.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum InGameState
+{
+    IDLE,
+    INGAME,
+    PAUSE
+}
+
+public enum InGamePause
+{
+    IDLE,
+    CHARACTER,
+    ORDER,
+    SETTINGS,
+    EXIT
+}
+
+
+public class InGameUI : MonoBehaviour
+{
+    public static InGameUI Instance;
+    public InGameState curState = InGameState.INGAME, lastState = InGameState.IDLE;
+    private InGamePause curSelection = InGamePause.IDLE, lastSelection = InGamePause.IDLE;
+
+    private Transform[] go;
+    void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Overlord InGame.");
+            Destroy(this);
+        }
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+        go = GetComponentsInChildren<Transform>(true);
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        switch (curState)
+        {
+            case InGameState.INGAME:
+                CheckObject("InGame", "MainMenu");
+                lastState = curState;
+                curState = InGameState.IDLE;
+                curSelection = InGamePause.IDLE;
+                lastSelection = InGamePause.IDLE;
+                break;
+            case InGameState.PAUSE:
+                CheckObject("MainMenu", "InGame");
+                lastState = curState;
+                curState = InGameState.IDLE;
+                break;
+            default:
+                break;
+        }
+
+        switch (curSelection)
+        {
+            case InGamePause.CHARACTER:
+                break;
+            case InGamePause.ORDER:
+                break;
+            case InGamePause.SETTINGS:
+                break;
+            case InGamePause.EXIT:
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    public InGameState GetLastState()
+    {
+        return lastState;
+    }
+
+    private void CheckObject(string name1, string name2)
+    {
+        if (go != null)
+        {
+            foreach (Transform t in go)
+            {
+                if (t.name == name1)
+                {
+                    t.gameObject.SetActive(true);
+                }
+                if (t.name == name2)
+                {
+                    t.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public void ActivateMM()
+    {
+        if(lastState != InGameState.PAUSE)
+            curState = InGameState.PAUSE;
+    }
+
+    public void DiactivateMM()
+    {
+        if (lastState != InGameState.INGAME)
+            curState = InGameState.INGAME;
+    }
+
+    public void SetGameState(InGameState state)
+    {
+        curState = state;
+    }
+
+    public void SetGameMenuState(InGamePause state)
+    {
+        curSelection = state;
+    }
+}
---
+++ Assets/Scripts/UI/DracoUI/Script/UI/MainMenuItemManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuItemManager : MonoBehaviour {
+    private Transform[] child, temp;
+    public static MainMenuItemManager Instance;
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Overlord.");
+            Destroy(this);
+        }
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        child = new Transform[3];
+        int i = 0;
+        temp = GetComponentsInChildren<Transform>(true);
+        
+        foreach(Transform t in temp)
+        {
+            if (t.tag.Equals("MainMenuItem"))
+            {
+                child[i] = t;
+
+                i++;
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    public Transform[] GetChild()
+    {
+        return child;
+    }
+
+    public GameObject GetSpecificChild(string n)
+    {
+        Transform temp = null;
+        foreach (Transform t in child)
+        {
+            if(t.name == n)
+            {
+                temp = t;
+            }
+        }
+        return temp.gameObject;
+    }
+}
