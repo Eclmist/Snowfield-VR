ff2933ef4a030138d444739c48c9789ea453e924
Samuel Ng <eclmist@yahoo.com>
2017-05-31 22:52:04 +0800

    Weapons now can charge
    Added emmisive glow and datastructure for weapon charging

--- Assets/Scripts/Items/Weapon.cs
+++ Assets/Scripts/Items/Weapon.cs
@@ -6,63 +6,128 @@ using System;
 public class Weapon : Equipment
 {
 
-    [SerializeField]
-    protected float range;
-    private Animator animator;
+	[SerializeField]
+	protected float range;
+	private Animator animator;
 
-    protected override void Awake()
-    {
-        base.Awake();
-        animator = GetComponent<Animator>();
-    }
-    public float Range
-    {
-        get
-        {
-            return range;
-        }
-        set
-        {
-            range = value;
-        }
-    }
+	[Header("Weapon Charge")]
+	[SerializeField] [Range(0, 20)] private float chargeDuration = 10;
+	[SerializeField] private AnimationCurve emissiveCurve;
+	[SerializeField] [ColorUsageAttribute(true,true,0f,8f,0.125f,3f)]
+	private Color emissiveColor;
 
-    private Action<IBlock> IfBlocked;
+	[SerializeField] [Range(0, 10)] private float fadeSpeed = 5;
+	[SerializeField] private XftWeapon.XWeaponTrail trail;
 
-    public void SetBlockable(Action<IBlock> ifblocked = null)
-    {
-        IfBlocked = ifblocked;
-    }
+	private float timeSinceStartCharge = 0;
+	private float emissiveSlider = 0;
+	private ModifyRenderer modRen;
+	private bool charge = true;
+	protected override void Awake()
+	{
+		base.Awake();
+		animator = GetComponent<Animator>();
+		modRen = GetComponent<ModifyRenderer>();
+	}
 
-    protected override void UseItem()
-    {
-        base.UseItem();
-    }
+	private void Start()
+	{
+		modRen = GetComponent<ModifyRenderer>();
+	}
 
-    protected override void OnTriggerEnter(Collider collision)
-    {
-        base.OnTriggerEnter(collision);
-        if (IfBlocked != null)
-        {
-            IBlock item = collision.GetComponentInParent<IBlock>();
-            if (item != null && item.IsBlocking)
-            {
-                IfBlocked(item);
-            }
-        }
-    }
+	protected void Update()
+	{
+		if (modRen)
+		{
 
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawSphere(transform.position, range);
-    }
+			if (charge)
+			{
+				if (emissiveSlider < 1)
+				{
+					emissiveSlider += fadeSpeed * Time.deltaTime;
+					if (emissiveSlider > 1)
+						emissiveSlider = 1;
+				}
 
-    public void StartCharge()
-    {
-        if(animator != null)
-        {
-            animator.SetTrigger("Charge");
-        }
-    }
+				timeSinceStartCharge += Time.deltaTime;
+			}
+			else
+			{
+				if (emissiveSlider > 0)
+				{
+					emissiveSlider -= fadeSpeed * Time.deltaTime;
+					if (emissiveSlider < 0)
+						emissiveSlider = 0;
+				}
 
+			}
+
+			Color targetColor = emissiveColor * emissiveCurve.Evaluate(emissiveSlider);
+			trail.MyColor = targetColor / 2;
+			modRen.SetColor("_EmissionColor", targetColor);
+
+			if (timeSinceStartCharge > chargeDuration + 1)
+				charge = false;
+		}
+	}
+
+	public float Range
+	{
+		get
+		{
+			return range;
+		}
+		set
+		{
+			range = value;
+		}
+	}
+
+	private Action<IBlock> IfBlocked;
+
+	public void SetBlockable(Action<IBlock> ifblocked = null)
+	{
+		IfBlocked = ifblocked;
+	}
+
+	protected override void UseItem()
+	{
+		base.UseItem();
+	}
+
+	protected override void OnTriggerEnter(Collider collision)
+	{
+		base.OnTriggerEnter(collision);
+		if (IfBlocked != null)
+		{
+			IBlock item = collision.GetComponentInParent<IBlock>();
+			if (item != null && item.IsBlocking)
+			{
+				IfBlocked(item);
+			}
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawSphere(transform.position, range);
+	}
+
+	public void StartCharge()
+	{
+		if (animator != null && timeSinceStartCharge > chargeDuration)
+		{
+			animator.SetTrigger("Charge");
+			timeSinceStartCharge = 0;
+			charge = true;
+		}
+	}
+
+	public void EndCharge()
+	{
+		if (timeSinceStartCharge > chargeDuration)
+		{
+			charge = false;
+		}
+	}
 }
