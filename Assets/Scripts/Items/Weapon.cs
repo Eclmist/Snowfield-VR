864756687fb752745432290117add57c3e278174
Chen Xiang <xMashedxTomatox@gmail.com>
2017-06-01 01:59:01 +0800

    Finished sword charge and attack

--- Assets/Scripts/Items/Weapon.cs
+++ Assets/Scripts/Items/Weapon.cs
@@ -6,128 +6,145 @@ using System;
 public class Weapon : Equipment
 {
 
-	[SerializeField]
-	protected float range;
+    [SerializeField]
+    protected float range;
 
-	[Header("Weapon Charge")]
-	[SerializeField] [Range(0, 20)] private float chargeDuration = 10;
-	[SerializeField] private AnimationCurve emissiveCurve;
-	[SerializeField] [ColorUsageAttribute(true,true,0f,8f,0.125f,3f)]
-	private Color emissiveColor;
+    [Header("Weapon Charge")]
+    [SerializeField]
+    [Range(0, 20)]
+    private float chargeDuration = 10;
+    [SerializeField]
+    private AnimationCurve emissiveCurve;
+    [SerializeField]
+    [ColorUsageAttribute(true, true, 0f, 8f, 0.125f, 3f)]
+    private Color emissiveColor;
 
-	[SerializeField] [Range(0, 10)] private float fadeSpeed = 5;
-	[SerializeField] private XftWeapon.XWeaponTrail trail;
+    [SerializeField]
+    [Range(0, 10)]
+    private float fadeSpeed = 5;
+    [SerializeField]
+    private XftWeapon.XWeaponTrail trail;
 
-	private float timeSinceStartCharge = 0;
-	private float emissiveSlider = 0;
-	private ModifyRenderer modRen;
-	private bool charge = false;
-	protected override void Awake()
-	{
-		base.Awake();
-		modRen = GetComponent<ModifyRenderer>();
-	}
+    private float timeSinceStartCharge = 0;
+    private float emissiveSlider = 0;
+    private ModifyRenderer modRen;
+    private bool charge = false;
+    protected override void Awake()
+    {
+        base.Awake();
+        modRen = GetComponent<ModifyRenderer>();
+    }
 
-	private void Start()
-	{
-		modRen = GetComponent<ModifyRenderer>();
+    private void Start()
+    {
+        modRen = GetComponent<ModifyRenderer>();
 
-	}
+    }
 
-	protected void Update()
-	{
-		if (modRen)
-		{
+    protected void Update()
+    {
+        if (modRen)
+        {
 
-			if (charge)
-			{
-				if (emissiveSlider < 1)
-				{
-					emissiveSlider += fadeSpeed * Time.deltaTime;
-					if (emissiveSlider > 1)
-						emissiveSlider = 1;
-				}
+            if (charge)
+            {
+                if (emissiveSlider < 1)
+                {
+                    emissiveSlider += fadeSpeed * Time.deltaTime;
+                    if (emissiveSlider > 1)
+                        emissiveSlider = 1;
+                }
 
-				timeSinceStartCharge += Time.deltaTime;
-			}
-			else
-			{
-				if (emissiveSlider > 0)
-				{
-					emissiveSlider -= fadeSpeed * Time.deltaTime;
-					if (emissiveSlider < 0)
-						emissiveSlider = 0;
-				}
+                timeSinceStartCharge += Time.deltaTime;
+            }
+            else
+            {
+                if (emissiveSlider > 0)
+                {
+                    emissiveSlider -= fadeSpeed * Time.deltaTime;
+                    if (emissiveSlider < 0)
+                        emissiveSlider = 0;
+                }
 
-			}
+            }
 
-			Color targetColor = emissiveColor * emissiveCurve.Evaluate(emissiveSlider);
+            Color targetColor = emissiveColor * emissiveCurve.Evaluate(emissiveSlider);
 
-			if (trail != null)
-				trail.MyColor = targetColor / 2;
-			modRen.SetColor("_EmissionColor", targetColor);
+            if (trail != null)
+                trail.MyColor = targetColor / 2;
+            modRen.SetColor("_EmissionColor", targetColor);
 
-			if (timeSinceStartCharge > chargeDuration + 1)
-				charge = false;
-		}
-	}
+            if (timeSinceStartCharge > chargeDuration + 1)
+                charge = false;
+        }
+    }
 
-	public float Range
-	{
-		get
-		{
-			return range;
-		}
-		set
-		{
-			range = value;
-		}
-	}
+    public float Range
+    {
+        get
+        {
+            return range;
+        }
+        set
+        {
+            range = value;
+        }
+    }
 
-	private Action<IBlock> IfBlocked;
+    private Action<IBlock> IfBlocked;
 
-	public void SetBlockable(Action<IBlock> ifblocked = null)
-	{
-		IfBlocked = ifblocked;
-	}
+    public void SetBlockable(Action<IBlock> ifblocked = null)
+    {
+        IfBlocked = ifblocked;
+    }
 
-	protected override void UseItem()
-	{
-		base.UseItem();
-	}
+    protected override void UseItem()
+    {
+        base.UseItem();
+    }
 
-	protected override void OnTriggerEnter(Collider collision)
-	{
-		base.OnTriggerEnter(collision);
-		if (IfBlocked != null)
-		{
-			IBlock item = collision.GetComponentInParent<IBlock>();
-			if (item != null && item.IsBlocking)
-			{
-				IfBlocked(item);
-			}
-		}
-	}
+    protected override void OnTriggerEnter(Collider collision)
+    {
+        base.OnTriggerEnter(collision);
 
-	//void OnDrawGizmos()
-	//{
-	//	Gizmos.DrawSphere(transform.position, range);
-	//}
+        if (IfBlocked != null)
+        {
+            if (LinkedController != null)
+            {
+                IDamagable target = collision.GetComponent<IDamagable>();
+                if (target != null)
+                {
+                    target.TakeDamage(Damage, Player.Instance);
+                }
+            }
 
-	public void StartCharge()
-	{
-		if (!charge)
-		{
-			timeSinceStartCharge = 0;
-			charge = true;
-		}
-	}
+            IBlock item = collision.GetComponentInParent<IBlock>();
+            if (item != null && item.IsBlocking)
+            {
+                IfBlocked(item);
+            }
+        }
+    }
 
-	public void EndCharge()
-	{
-		if (timeSinceStartCharge > chargeDuration)
-		{
-			charge = false;
-		}
-	}
+    //void OnDrawGizmos()
+    //{
+    //	Gizmos.DrawSphere(transform.position, range);
+    //}
+
+    public void StartCharge()
+    {
+        if (!charge)
+        {
+            timeSinceStartCharge = 0;
+            charge = true;
+        }
+    }
+
+    public void EndCharge()
+    {
+        if (timeSinceStartCharge > chargeDuration)
+        {
+            charge = false;
+        }
+    }
 }
