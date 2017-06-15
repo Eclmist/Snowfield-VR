255971464d2ac5763cb3fb685a759af490ca2489
Unknown <marcusheadgotscarm16@gmail.com>
2017-06-15 11:16:02 +0800

    Basic item editor functionalities
    - created ItemData.cs
    - moved some genericItem properties to ItemData
    - made itemManager a prefab

--- Assets/ThoughtBubble.cs
+++ Assets/ThoughtBubble.cs
@@ -10,22 +10,22 @@ public class ThoughtBubble : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Start () {
+	//void Start () {
 
-        preview = GetComponent<Image>();
+ //       preview = GetComponent<Image>();
 
-	}
+	//}
 	
-	// Update is called once per frame
-	void Update () {
+	//// Update is called once per frame
+	//void Update () {
 
-        if (DimensionBag.Instance.GetSelectedItem() != null)
-            preview.sprite = DimensionBag.Instance.GetSelectedItem().Icon;
-        else
-            preview.sprite = defaultPreview;
+ //       if (DimensionBag.Instance.GetSelectedItem() != null)
+ //           preview.sprite = DimensionBag.Instance.GetSelectedItem().Icon;
+ //       else
+ //           preview.sprite = defaultPreview;
         
 
-	}
+	//}
 
 
 
