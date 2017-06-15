b9d823f514272ffc9c861963e4c21b1f5292229c
Unknown <marcusheadgotscarm16@gmail.com>
2017-06-15 14:51:03 +0800

    Created Simple Inventory Structure

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Inventory
+{
+    
+
+    public class InventorySlot
+    {
+        int currentStack;
+        IStorable storedItem;
+
+        public InventorySlot(IStorable item,int quantity)
+        {
+            currentStack = quantity;
+            storedItem = item;
+        }
+
+        public InventorySlot(IStorable item)
+        {
+            currentStack = 1;
+            storedItem = item;
+        }
+
+        public int CurrentStack
+        {
+            get { return this.currentStack; }
+            set { this.currentStack = value; }
+        }
+
+        public IStorable StoredItem
+        {
+            get { return this.storedItem; }
+            set { this.storedItem = value; }
+        }
+
+    }
+
+    public Inventory(int maxSlots)
+    {
+        inventoryItems = new InventorySlot[maxSlots];
+    }
+
+    private InventorySlot[] inventoryItems;
+    
+    
+    public InventorySlot[] InventoryItems
+    {
+        get { return this.inventoryItems; }
+    }
+
+
+    public void AddToInventory(IStorable item)
+    {
+
+        bool added = false;
+
+        foreach (InventorySlot slot in inventoryItems)
+        {
+            if (slot.CurrentStack == slot.StoredItem.MaxStackSize)
+                continue;
+
+            if(item.ItemID == slot.StoredItem.ItemID)
+            {
+                slot.CurrentStack++;
+            }
+        }
+
+        // Add item to an empty slot
+        if(!added)
+        {
+            foreach(InventorySlot slot in inventoryItems)
+            {
+                if(slot.StoredItem == null)
+                {
+                    slot.StoredItem = item;
+                }
+            }
+        }
+
+    }
+
+    public GameObject RetrieveItem(int index)
+    {
+        if (index < inventoryItems.Length)
+            return inventoryItems[index].StoredItem.ObjectReference;
+        else
+            return null;
+    }
+
+
+
+
+
+
+
+}
