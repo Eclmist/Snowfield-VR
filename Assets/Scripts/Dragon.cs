84f183f2b0461b6885d7bfe3e6e2037a779d4fe3
Unknown <marcusheadgotscarm16@gmail.com>
2017-06-15 17:20:48 +0800

    Imported Dragon and Spider

---
+++ Assets/Scripts/Dragon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour {
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.forward * moveSpeed;
+	}
+}
