40aa6b7c1028579ca0ae71f1517ead6687d34109
Unknown <marcusheadgotscarm16@gmail.com>
2017-06-15 18:15:04 +0800

    Created StoragePanel

---
+++ Assets/Scripts/Inventory/StoragePanel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoragePanel : GenericItem {
+
+    [SerializeField]
+    private int inventorySize;
+
+    private Inventory inventory;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        inventory = new Inventory(inventorySize);
+	}
+	
+	
+
+    void UpdateInventoryItems()
+    {
+        foreach(Inventory.InventorySlot slot in inventory.InventoryItems)
+        {
+            
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+}
