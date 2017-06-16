93f920e2a504453c5b44623b489040adac06ae0e
Samuel Ng <eclmist@yahoo.com>
2017-06-16 18:47:55 +0800

    ItemHighlight Prototype

---
+++ Assets/Graphics/Post Process/Scripts/GenericPostProcess.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[ImageEffectAllowedInSceneView]
+[RequireComponent(typeof(Camera))]
+public class GenericPostProcess : MonoBehaviour
+{
+	[SerializeField] private Shader shader;
+
+	private Camera camera;
+	private Material mat;
+
+	protected void Awake()
+	{
+		camera = GetComponent<Camera>();
+		mat = new Material(shader);
+
+		Debug.Assert(mat);
+	}
+
+	protected void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		Graphics.Blit(source, destination, mat);
+	}
+
+}
