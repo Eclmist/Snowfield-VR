c3203e4b7fc643a8c46fd7b9dade9481b400c75b
Samuel Ng <eclmist@yahoo.com>
2017-06-16 18:47:55 +0800

    ItemHighlight Prototype

---
+++ Assets/Graphics/Post Process/Scripts/OutlineRenderer.cs
@@ -0,0 +1,80 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ImageEffectAllowedInSceneView]
+[RequireComponent(typeof(Camera))]
+public class OutlineRenderer : MonoBehaviour
+{
+	[SerializeField] private Shader shader;
+	[SerializeField] private Shader replacement;
+	//[SerializeField] private Shader blur;
+
+
+	//[SerializeField] [Range(0,4)] private int blurIteration = 4;
+	//[SerializeField] [Range(1, 4)] private int downSample = 2;
+
+	[SerializeField] private Vector2 offset = new Vector2(1,1);
+
+	private Camera camera;
+	private Camera tempSecondaryCamera;
+	private Material mat;
+	//private Material blurMat;
+	RenderTexture TempRT;
+
+	protected void OnEnable()
+	{
+		camera = GetComponent<Camera>();
+		tempSecondaryCamera = new GameObject().AddComponent<Camera>();
+		tempSecondaryCamera.enabled = false;
+		Debug.Assert(replacement);
+		mat = new Material(shader);
+		Debug.Assert(mat);
+		//blurMat = new Material(blur);
+		//Debug.Assert(blurMat);
+
+		TempRT = new RenderTexture(camera.pixelWidth, camera.pixelHeight, 0);
+
+	}
+
+	protected void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		//set up a temporary camera
+
+		tempSecondaryCamera.CopyFrom(camera);
+		tempSecondaryCamera.clearFlags = CameraClearFlags.Color;
+		tempSecondaryCamera.backgroundColor = Color.black;
+		tempSecondaryCamera.SetReplacementShader(replacement, "Outline");
+
+		//make the temporary rendertexture
+
+		//set the camera's target texture when rendering
+		tempSecondaryCamera.targetTexture = TempRT;
+
+		tempSecondaryCamera.Render();
+
+		//for (int i = 0; i < blurIteration; i++)
+		//{ }
+
+		//RenderTexture blurredTex = new RenderTexture(source.width / downSample, source.height / downSample, 0);
+		mat.SetVector("_offset", offset);
+
+		//Graphics.Blit(TempRT, blurredTex, blurMat);
+
+
+		mat.SetTexture("_OutlineBufferTex", TempRT);
+		//mat.SetTexture("_OutlineBufferTexBlurred", blurredTex);
+
+		Graphics.Blit(source, destination, mat);
+
+		//blurredTex.Release();
+	}
+
+
+	protected void OnDisable()
+	{
+		TempRT.Release();
+	}
+}
+
---
+++ Assets/Graphics/Shaders/SubShader/Unlit.shader
@@ -0,0 +1,48 @@
+﻿Shader "SP/Outline/UnlitBase"
+{
+	Properties
+	{
+		_OutlineColor("Outline Color", Color) = (1,1,1,1)
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" "Queue"="Transparent" "Outline"="true"}
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			float4 _OutlineColor;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				return _OutlineColor;
+			}
+			ENDCG
+		}
+	}
+}
