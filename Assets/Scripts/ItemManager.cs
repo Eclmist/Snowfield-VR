e9023fbb02b75c5c3f0f207654756614eb8e51be
jkbhk <marcusheadgotscarm16@gmail.com>
2017-06-28 11:41:42 +0800

    not able to ask itemManager if an item is unlocked

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -68,6 +68,13 @@ public class ItemManager : MonoBehaviour {
         }
     }
 
+    public bool IsUnlocked(ItemData item)
+    {
+        return (item.LevelUnlocked >= Player.Instance.GetJob(item.ObjectReference.GetComponent<GenericItem>().JobType).Level);   
+    }
+    
+    
+
 
  
 }
