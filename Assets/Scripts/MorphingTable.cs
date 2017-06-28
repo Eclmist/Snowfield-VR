a6f50d14c592a4c23c171eb0807f760e97976732
jkbhk <marcusheadgotscarm16@gmail.com>
2017-06-28 20:43:34 +0800

    Created a morphing table

---
+++ Assets/Scripts/MorphingTable.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class MorphingTable : MonoBehaviour {
+
+    public Transform lockTransform;
+    private bool isOccupied;
+    private Ingot ingotReference;
+
+    void Start()
+    {
+        GetComponent<SphereCollider>().isTrigger = true;    
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        isOccupied = (ingotReference.transform.position == lockTransform.position
+                        && ingotReference.transform.rotation == lockTransform.rotation) ||
+                        (ingotReference != null);
+        
+
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Ingot ingot = other.gameObject.GetComponent<Ingot>();
+
+        if (!isOccupied && ingot != null)
+        {
+            ingotReference = ingot;
+
+            if (ingot.LinkedController != null)
+                ingot.OnControllerExit(ingot.LinkedController);
+
+            ingot.GetComponent<Rigidbody>().isKinematic = true;
+            LockIntoPosition(ingot.transform);
+        }
+    }
+
+
+    public void LockIntoPosition(Transform t)
+    {
+        t.position = lockTransform.position;
+        t.rotation = lockTransform.rotation;
+    }
+
+
+
+
+    
+
+
+
+
+
+
+
+
+}
