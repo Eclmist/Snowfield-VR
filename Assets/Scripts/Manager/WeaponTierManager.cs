b9c2f4f54d69f4fc96a1675fc26adb96e9f9e2fc
jkbhk <marcusheadgotscarm16@gmail.com>
2017-06-28 14:53:59 +0800

    Created a WeaponTierManager

---
+++ Assets/Scripts/Manager/WeaponTierManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponTierManager : MonoBehaviour {
+
+
+    // Weapon Classes are grouped based on Type
+    [System.Serializable]
+    public class WeaponClass
+    {
+        [SerializeField]
+        private TYPE type;
+
+        [SerializeField][Header("Item ID")]
+        private List<int> tierList;
+
+
+    }
+
+    public static WeaponTierManager Instance;
+    public List<WeaponClass> weaponClassList = new List<WeaponClass>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Awake()
+    {
+        Instance = this;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+}
