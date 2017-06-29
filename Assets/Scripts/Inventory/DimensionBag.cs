b6947edf602b4818b1c79b9a0496709a6e62fd70
Chen Xiang <xmashedxtomatox@gmail.com>
2017-06-29 20:59:46 +0800

    Basic item editor functionalities
    - created ItemData.cs
    - moved some genericItem properties to ItemData
    - made itemManager a prefab

--- Assets/Scripts/Inventory/DimensionBag.cs
+++ Assets/Scripts/Inventory/DimensionBag.cs
@@ -4,70 +4,78 @@
 //using UnityEngine;
 //using UnityEngine.UI;
 
-//public class DimensionBag : GenericItem {
+public class DimensionBag : GenericItem {
 
-//    public class DimensionSlot
-//    {
-//        int stackSize;
-//        GenericItem item;
+    public class DimensionSlot
+    {
+        int stackSize;
+        GenericItem item;
 
-//        public DimensionSlot(GenericItem item)
-//        {
+        public DimensionSlot(GenericItem item)
+        {
             
-//            this.stackSize = 1;
-//            this.item = item;
-//        }
+            this.stackSize = 1;
+            this.item = item;
+        }
 
-//        public int StackSize { get; set; }
-//        public GenericItem Item { get; set; }
+        public int StackSize { get; set; }
+        public GenericItem Item { get; set; }
 
-//    }
+    }
+
+    public static DimensionBag Instance;
+    public GameObject thoughtBubble;
+
+    private List<DimensionSlot> dimensionItems;
+    private int itor = 0;
+    private bool isInside;
+
+    [SerializeField]
+    [Range(1f,5f)]
+    private float distanceToReact;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        Instance = this;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        dimensionItems = new List<DimensionSlot>();
 
 //    public static DimensionBag Instance;
 //    public GameObject thoughtBubble;
 
-//    private List<DimensionSlot> dimensionItems;
-//    private int itor = 0;
-//    private bool isInside;
+        AddItemToDimension(ItemManager.Instance.GetItem(12));
+        AddItemToDimension(ItemManager.Instance.GetItem(12));
 
-//    [SerializeField]
-//    [Range(1f,5f)]
-//    private float distanceToReact;
-
-//    protected override void Awake()
-//    {
-//        base.Awake();
-//        Instance = this;
-//    }
-
-//	// Use this for initialization
-//	void Start ()
-//    {
-//        dimensionItems = new List<DimensionSlot>();
-
-
-//        AddItemToDimension(ItemManager.Instance.GetItem(12));
-//        AddItemToDimension(ItemManager.Instance.GetItem(12));
-
-//    }
+    }
 	
-//    void Update()
-//    {
-//        if (Vector3.Distance(Player.Instance.transform.position,transform.position ) < distanceToReact)
-//        {
-//            // Play animation
+    void Update()
+    {
+        if (Vector3.Distance(Player.Instance.transform.position,transform.position ) < distanceToReact)
+        {
+            // Play animation
 
-//        }
+        }
 
-//        thoughtBubble.SetActive(isInside);
+        thoughtBubble.SetActive(isInside);
 
 
-//        Debug.Log(isInside);
+        Debug.Log(isInside);
         
         
 
 
-//    }
+    }
+
+    public List<DimensionSlot> Items
+    {
+        get { return this.dimensionItems; }
+        set { this.dimensionItems = value; }
+    }
 
 //    public List<DimensionSlot> Items
 //    {
@@ -75,14 +83,26 @@
 //        set { this.dimensionItems = value; }
 //    }
 
+    public void AddItemToDimension(GenericItem item)
+    {
+        bool added = false;
 
-//    public void AddItemToDimension(GenericItem item)
-//    {
-//        bool added = false;
+        if(item != null)
+        {
 
 //        if(item != null)
 //        {
 
+            // Check for existing items to stack
+            foreach (DimensionSlot d in dimensionItems)
+            {
+                if (item.ID == d.Item.ID && d.StackSize < d.Item.MaxStackSize)
+                {
+                    d.StackSize++;
+                    added = true;
+                    break;
+                }
+            }
 
 //            // Check for existing items to stack
 //            foreach (DimensionSlot d in dimensionItems)
@@ -95,27 +115,32 @@
 //                }
 //            }
 
+            if(!added)
+            {
+                DimensionSlot slot = new DimensionSlot(item);
+                slot.Item = item;
+                slot.StackSize = 1;
+                dimensionItems.Add(slot);
+            }
 
-//            if(!added)
-//            {
-//                DimensionSlot slot = new DimensionSlot(item);
-//                slot.Item = item;
-//                slot.StackSize = 1;
-//                dimensionItems.Add(slot);
-//            }
-
-//        }
+        }
             
-//    }
+    }
 
-//    public void RetrieveItemFromDimension()
-//    {
+    public void RetrieveItemFromDimension()
+    {
 
-//        DimensionSlot d = dimensionItems[itor];
-//        d.StackSize--;
+        DimensionSlot d = dimensionItems[itor];
+        d.StackSize--;
 
-//        GameObject g = Instantiate(d.Item.objReference);
-//        g.GetComponent<GenericItem>().StartInteraction(LinkedController);
+        GameObject g = Instantiate(d.Item.objReference);
+        g.GetComponent<GenericItem>().StartInteraction(LinkedController);
+
+        if (d.StackSize < 1)
+        {
+            dimensionItems.Remove(d);
+            itor = 0;
+        }
 
 //        if (d.StackSize < 1)
 //        {
@@ -125,84 +150,210 @@
 
         
 
-//    }
+    }
 
-//    public GenericItem GetSelectedItem()
-//    {
+    public GenericItem GetSelectedItem()
+    {
 
-//        if (dimensionItems.Count < 1)
-//            return null;
-//        else
-//            return dimensionItems[itor].Item;
+        if (dimensionItems.Count < 1)
+            return null;
+        else
+            return dimensionItems[itor].Item;
 
-//    }
+    }
 
-//    void OnTriggerStay(Collider other)
-//    {
-//        if(other.gameObject.GetComponent<VR_Controller_Custom>() != null)
-//        {
-//            isInside = true;
-//        }
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.GetComponent<VR_Controller_Custom>() != null)
+        {
+            isInside = true;
+        }
          
-//    }
+    }
 
-//    void OnTriggerExit(Collider other)
-//    {
-//        if (other.gameObject.GetComponent<VR_Controller_Custom>() != null)
-//        {
-//            isInside = false;
-//        }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<VR_Controller_Custom>() != null)
+        {
+            isInside = false;
+        }
             
-//    }
+    }
 
-//    public override void Interact(VR_Controller_Custom referenceCheck)
-//    {
-
-
-//        if(isInside)
-//        {
-//            if (referenceCheck.Device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
-//            {
-//                Vector2 touchpad = (referenceCheck.Device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0));
-
-//                if (referenceCheck.Device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0)[0] > 0.6f
-//                && referenceCheck.Device.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad)
-//                && itor < dimensionItems.Count-1)
-//                {
-//                    itor++;
-//                }
-
-//                if (referenceCheck.Device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0)[0] < -0.6f
-//                && referenceCheck.Device.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad)
-//                && itor > 0)
-//                {
-//                    itor--;
-//                }
-
-//            }
+    public override void Interact(VR_Controller_Custom referenceCheck)
+    {
 
 
-//            if (referenceCheck.Device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger) && dimensionItems.Count > 0)
-//            {
-//                this.audioSource.Play();
-//                StartInteraction(referenceCheck);
-//            }
-//        }
+        if(isInside)
+        {
+            if (referenceCheck.Device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                Vector2 touchpad = (referenceCheck.Device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0));
+
+                if (referenceCheck.Device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0)[0] > 0.6f
+                && referenceCheck.Device.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad)
+                && itor < dimensionItems.Count-1)
+                {
+                    itor++;
+                }
+
+                if (referenceCheck.Device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0)[0] < -0.6f
+                && referenceCheck.Device.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad)
+                && itor > 0)
+                {
+                    itor--;
+                }
+
+            }
+
+
+            if (referenceCheck.Device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger) && dimensionItems.Count > 0)
+            {
+                this.audioSource.Play();
+                StartInteraction(referenceCheck);
+            }
+        }
         
 
-//    }
+    }
 
 
-//    public override void StartInteraction(VR_Controller_Custom referenceCheck)
-//    {
-//        base.StartInteraction(referenceCheck);
-//        RetrieveItemFromDimension();
+    public override void StartInteraction(VR_Controller_Custom referenceCheck)
+    {
+        base.StartInteraction(referenceCheck);
+        RetrieveItemFromDimension();
 
-//    }
+    }
 
-//    void OnDrawGizmos()
-//    {
-//        Gizmos.DrawWireSphere(transform.position,distanceToReact);
-//    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position,distanceToReact);
+    }
 
 //}
+
+//public class DimensionBag : GenericItem {
+//    public class DimensionSlot
+//    {
+//        int stackSize;
+//        GenericItem item;
+
+//        public DimensionSlot(GenericItem item)
+//        {
+//            this.stackSize = 1;
+//            this.item = item;
+//        }
+//        public int StackSize { get; set; }
+//        public GenericItem Item { get; set; }
+//    }
+//    private List<DimensionSlot> dimensionItems;
+//    private int itor = 0;
+//    private bool isInside;
+//    [SerializeField]
+//    [Range(1f,5f)]
+//    private float distanceToReact;
+//    protected override void Awake()
+//    {
+//        base.Awake();
+//        Instance = this;
+//    }
+//	// Use this for initialization
+//	void Start ()
+//    {
+//        dimensionItems = new List<DimensionSlot>();
+//        AddItemToDimension(ItemManager.Instance.GetItem(12));
+//        AddItemToDimension(ItemManager.Instance.GetItem(12));
+
+//    }
+	
+//    void Update()
+//    {
+//        if (Vector3.Distance(Player.Instance.transform.position,transform.position ) < distanceToReact)
+//        {
+//            // Play animation
+
+//        }
+
+//        thoughtBubble.SetActive(isInside);
+
+
+//        Debug.Log(isInside);
+//    }
+//    public void AddItemToDimension(GenericItem item)
+//    {
+//        bool added = false;
+//            if(!added)
+//            {
+//                DimensionSlot slot = new DimensionSlot(item);
+//                slot.Item = item;
+//                slot.StackSize = 1;
+//                dimensionItems.Add(slot);
+//            }
+
+//        }
+//    }
+//    public void RetrieveItemFromDimension()
+//    {
+//        DimensionSlot d = dimensionItems[itor];
+//        d.StackSize--;
+//        GameObject g = Instantiate(d.Item.objReference);
+//        g.GetComponent<GenericItem>().StartInteraction(LinkedController);
+//    }
+//    public GenericItem GetSelectedItem()
+//    {
+//        if (dimensionItems.Count < 1)
+//            return null;
+//        else
+//            return dimensionItems[itor].Item;
+//    }
+
+//    void OnTriggerStay(Collider other)
+//    {
+//        if(other.gameObject.GetComponent<VR_Controller_Custom>() != null)
+//        {
+//            isInside = true;
+//        }
+//    }
+//    void OnTriggerExit(Collider other)
+//    {
+//        if (other.gameObject.GetComponent<VR_Controller_Custom>() != null)
+//        {
+//            isInside = false;
+//        }
+//    }
+//    public override void Interact(VR_Controller_Custom referenceCheck)
+//    {
+//        if(isInside)
+//        {
+//            if (referenceCheck.Device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+//            {
+//                Vector2 touchpad = (referenceCheck.Device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0));
+//                if (referenceCheck.Device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0)[0] > 0.6f
+//                && referenceCheck.Device.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad)
+//                && itor < dimensionItems.Count-1)
+//                {
+//                    itor++;
+//                }
+//                if (referenceCheck.Device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0)[0] < -0.6f
+//                && referenceCheck.Device.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad)
+//                && itor > 0)
+//                {
+//                    itor--;
+//                }
+//            }
+//            if (referenceCheck.Device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger) && dimensionItems.Count > 0)
+//            {
+//                this.audioSource.Play();
+//                StartInteraction(referenceCheck);
+//            }
+//        }
+//    }
+//    public override void StartInteraction(VR_Controller_Custom referenceCheck)
+//    {
+//        base.StartInteraction(referenceCheck);
+//        RetrieveItemFromDimension();
+//    }
+//    void OnDrawGizmos()
+//    {
+//        Gizmos.DrawWireSphere(transform.position,distanceToReact);
+//    }
---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ItemData {
+
+    [SerializeField]
+    private int ID;
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private bool isUnlocked;
+    [SerializeField]
+    private Sprite icon;
+    [SerializeField]
+    private int maxStackSize = 1;
+
+
+
+    public int ItemID
+    {
+        get { return this.ID; }
+    }
+
+    public GameObject ObjectReference
+    {
+        get { return this.prefab; }
+    }
+
+    public bool IsUnlocked
+    {
+        get { return this.isUnlocked; }
+        set { this.isUnlocked = value; }
+    }
+
+    public Sprite Icon
+    { 
+        get { return this.icon; }
+    }
+
+
+
+    public int MaxStackSize
+    {
+        get { return this.maxStackSize; }
+    }
+
+
+
+}
