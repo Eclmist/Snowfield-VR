be348760dcaea870967664c7b693aef2d2e6c089
jkbhk <marcusheadgotscarm16@gmail.com>
2017-07-27 14:54:00 +0800

    Created an interactable message object

---
+++ Assets/Scripts/MessageInbox/InteractableMessage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableMessage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private MessageManager.Mail storedMail;
+
+    public MessageManager.Mail StoredMail
+    {
+        get { return this.storedMail; }
+        set { this.storedMail = value; }
+    }
+
+    public void SetMessageReference()
+    {
+        if(storedMail != null)
+            MessageManager.Instance.DisplayMail(storedMail);
+    }
+}
