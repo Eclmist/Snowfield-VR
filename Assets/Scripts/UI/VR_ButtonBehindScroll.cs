14aa32d09063f1a5a3df3b8db343f1f7adc89e8a
jkbhk <marcusheadgotscarm16@gmail.com>
2017-07-27 14:54:44 +0800

    Created a new type of VR_Button

---
+++ Assets/Scripts/UI/VR_ButtonBehindScroll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR_ButtonBehindScroll : VR_Button {
+
+    // If the distance between the last recorded trigger press and release is greater than this,
+    // the button will not trigger
+    [SerializeField] protected float distanceMoveThreshold;
+    private Vector3 start;
+    private bool clicked = false;
+
+    
+
+    protected override void OnTriggerPress()
+    {
+        start = currentInteractingController.transform.position;
+        clicked = true;
+    }
+
+
+    protected override void OnTriggerRelease()
+    {
+        if(Vector3.Distance(currentInteractingController.transform.position,start) < distanceMoveThreshold && clicked)
+        {
+            clicked = false;
+            base.OnTriggerRelease();
+        }
+    }
+
+    
+
+    
+
+
+}
