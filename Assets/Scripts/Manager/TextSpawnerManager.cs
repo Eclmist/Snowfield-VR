ca5348e30c23cde6be70c3779b600daf608223ef
Unknown <marcusheadgotscarm16@gmail.com>
2017-07-29 00:53:17 +0800

    Created a Damage/Multi-purpose Indicator !

---
+++ Assets/Scripts/Manager/TextSpawnerManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextSpawnerManager : MonoBehaviour {
+
+    [SerializeField]
+    private SpawnedText textPrefab;
+    [SerializeField]
+    private Vector3 offset = new Vector3(0f,0.2f,0f);
+
+
+    public static TextSpawnerManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+	
+    public void SpawnText(string text, Color color, Transform t, Collider c)
+    {
+        Debug.Log("spawned");
+        SpawnedText st = Instantiate(textPrefab, (t.position + t.up * (c.bounds.size.y) + offset), t.rotation);
+        Debug.Log(c.bounds.extents.y);
+        st.SetText(text);
+        st.SetColor(color);
+    }
+
+    public void SpawnText(string text, Color color, Transform t, Collider c, int scale)
+    {
+        Debug.Log("spawned");
+        SpawnedText st = Instantiate(textPrefab, (t.position + t.up * (c.bounds.size.y) + offset), t.rotation);
+        st.transform.localScale *= scale;
+        Debug.Log(c.bounds.extents.y);
+        st.SetText(text);
+        st.SetColor(color);
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/UI/SpawnedText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnedText : MonoBehaviour {
+
+    [SerializeField]
+    private Text text;
+    [SerializeField]
+    private Animator anim;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        Destroy(this.gameObject,anim.GetCurrentAnimatorClipInfo(0).Length);
+	}
+	
+
+
+    public void SetText(string s)
+    {
+        text.text = s;
+    }
+
+    public void SetColor(Color c)
+    {
+        text.color = c;
+    }
+}
