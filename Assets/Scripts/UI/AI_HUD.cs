9fe1d8cc001ae4239d4e854dc8f9725929106c39
jkbhk <marcusheadgotscarm16@gmail.com>
2017-07-31 15:17:39 +0800

    changed ai hud to contain 2 layers for healthbar

--- Assets/Scripts/UI/AI_HUD.cs
+++ Assets/Scripts/UI/AI_HUD.cs
@@ -13,6 +13,7 @@ public struct HUDColorSettings
 	[ColorUsageAttribute(true, true, 0f, 8f, 0.125f, 3f)]
 	public Color lowHealth;
 
+
 }
 
 public class AI_HUD : MonoBehaviour {
@@ -20,6 +21,7 @@ public class AI_HUD : MonoBehaviour {
 	[SerializeField] private Text playerName;
 	[SerializeField] private GameObject healthbarValueObj;
 	[SerializeField] private GameObject playerIndicatorObj;
+    [SerializeField] private GameObject damageIndicatorObj;
     [SerializeField]
     private Text levelText;
 	[Space(10f)]
@@ -28,6 +30,8 @@ public class AI_HUD : MonoBehaviour {
 	
 	private Material playerIndicatorMatInstance;
 	private Material healthbarMatInstance;
+    private Material damagebarMatInstance;
+
 
 	private AdventurerAI actor = null;
 
@@ -48,8 +52,11 @@ public class AI_HUD : MonoBehaviour {
 		if (healthbarValueObj && playerIndicatorObj)
 		{
 			playerIndicatorMatInstance = playerIndicatorObj.GetComponent<Renderer>().material;
-			healthbarMatInstance = healthbarValueObj.GetComponent<Renderer>().material;
-		}
+            healthbarMatInstance = healthbarValueObj.GetComponent<Renderer>().material;
+            damagebarMatInstance = damageIndicatorObj.GetComponent<Renderer>().material;
+
+
+        }
 		else
 		{
 			Debug.LogError("Healthbar object not set, HUD prefab is likely broken. Self destructing...");
@@ -68,19 +75,20 @@ public class AI_HUD : MonoBehaviour {
 		// Set healthbar color
 		float health01 = 1 - (float)actor.Health / actor.MaxHealth;
 
-		Color currentHealthbarColor = healthbarMatInstance.GetColor("_TintColor");
+		Color currentHealthbarColor = damagebarMatInstance.GetColor("_TintColor");
 
 		Color targetColor = Color.Lerp(currentHealthbarColor,
 			Color.Lerp(colorSettings.fullHealth, colorSettings.lowHealth, health01),
 			Time.deltaTime * lerpSpeed);
 		targetColor.a = currentHealthbarColor.a;
 
-		healthbarMatInstance.SetColor("_TintColor", targetColor);
+        damagebarMatInstance.SetColor("_TintColor", targetColor);
 
-		// Set healthbar length
+        // Set healthbar length
+        healthbarMatInstance.SetTextureOffset("_MainTex", new Vector2(health01 * -1, 0));
 
-		healthbarMatInstance.SetTextureOffset("_MainTex", Vector2.Lerp(
-			healthbarMatInstance.GetTextureOffset("_MainTex"),
+        damagebarMatInstance.SetTextureOffset("_MainTex", Vector2.Lerp(
+            damagebarMatInstance.GetTextureOffset("_MainTex"),
 			new Vector2(health01 * -1, 0),
 			Time.deltaTime * lerpSpeed));
 
