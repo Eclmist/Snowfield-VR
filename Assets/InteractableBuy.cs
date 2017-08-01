c59d46c47411d3186c5e168d36555bb9676f9634
jkbhk <marcusheadgotscarm16@gmail.com>
2017-08-01 16:00:57 +0800

    done merchant structure

---
+++ Assets/InteractableBuy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableBuy : MonoBehaviour {
+
+    private ItemData itemData;
+
+    public ItemData _ItemData
+    {
+        get { return this.itemData; }
+        set { this.itemData = value; }
+    }
+
+    public void SelectItem()
+    {
+        Merchant.Instance.BuyItem(itemData);
+    }
+	
+	
+}
---
+++ Assets/Scripts/Merchant.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Merchant : MonoBehaviour
+{
+    public static Merchant Instance;
+
+    [SerializeField]
+    private GameObject interactableBuy;
+    [SerializeField]
+    private GridLayoutGroup layoutGroup;
+    
+    [SerializeField]
+    private List<GenericItem> buyableGenericItems;
+    private List<ItemData> buyableItemData;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        buyableItemData = new List<ItemData>();
+        PopulateBuyableItemDataList();
+        CreateCatalog();
+	}
+	
+
+    private void PopulateBuyableItemDataList()
+    {
+        foreach(GenericItem g in buyableGenericItems)
+        {
+            ItemData data = ItemManager.Instance.GetItemData(g.gameObject);
+
+            if(data != null)
+                buyableItemData.Add(data);
+        }
+    }
+    
+    private void CreateCatalog()
+    {
+        foreach(GenericItem g in buyableGenericItems)
+            Instantiate(interactableBuy, layoutGroup.transform, false);
+    }
+
+    public void BuyItem(ItemData data)
+    {
+       GameManager.Instance.AddPlayerGold(-data.Cost);
+       TextSpawnerManager.Instance.SpawnText("-" + data.Cost.ToString(),Color.red,transform);
+        StoragePanel.Instance._Inventory.AddToInventory(data);
+    }
+
+
+}
