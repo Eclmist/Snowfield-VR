9ec6cec8a96d3fb81888f5300b67bfaaccad7d4d
Unknown <marcusheadgotscarm16@gmail.com>
2017-08-06 20:01:14 +0800

    Added sound effect text

--- Assets/Scripts/Manager/TextSpawnerManager.cs
+++ Assets/Scripts/Manager/TextSpawnerManager.cs
@@ -18,6 +18,7 @@ public class TextSpawnerManager : MonoBehaviour {
         Instance = this;
     }
 	
+    // Generates a text that floats above the given transform and fades away
     public void SpawnText(string text, Color color, Transform t)
     {
         Collider c = t.GetComponent<Collider>();
@@ -27,10 +28,10 @@ public class TextSpawnerManager : MonoBehaviour {
             st = Instantiate(textPrefab, (t.position + t.up * (c.bounds.size.y) + offset), t.rotation);
         else
             st = Instantiate(textPrefab, (t.position + t.up * + offset.y), t.rotation);
-     
 
         st.SetText(text);
         st.SetColor(color);
+
     }
 
 
@@ -47,6 +48,60 @@ public class TextSpawnerManager : MonoBehaviour {
         st.transform.localScale *= scale;
         st.SetText(text);
         st.SetColor(color);
+    }
+
+
+    // Generates a text that stays at a position 
+    public void SpawnSoundEffect(string text, Color color, Transform t, Vector3 offset, float scale = 1)
+    {
+
+        
+        SpawnedText st = Instantiate(textPrefab,t.position + offset,Quaternion.identity);
+
+        st.transform.localScale *= scale;
+        st.SetText(text);
+        st.SetColor(color);
+        st.GetComponentInChildren<Animator>().enabled = false;
+        st.GetComponentInChildren<Outline>().enabled = true;
+        StartCoroutine(Shake(st.gameObject, 3, 0.05f));
+
+    }
+
+
+    private IEnumerator Shake(GameObject g, float shakeDuration, float shakeIntensity)
+    {
+
+        float timeElapsed = 0.0f;
+
+        Vector3 originalPos = g.transform.position;
+
+        while (timeElapsed < shakeDuration)
+        {
+
+            timeElapsed += Time.deltaTime;
+
+
+            // As the shake approaches the end, the smooth value increases
+            // The smooth value will be multiplied to the shake intensity
+            // This will slowly take away the shake intensty over time so it doesn't look like it snaps
+            float percentComplete = timeElapsed / shakeDuration;
+            float smooth = 1.0f - Mathf.Clamp(percentComplete, 0.0f, 1.0f);
+
+            // Multiplying by ( *2 - 1 ) gets a range from -1 to 1
+            float x = Random.value * 2.0f - 1.0f;
+            float y = Random.value * 2.0f - 1.0f;
+
+            x *= shakeIntensity * smooth;
+            y *= shakeIntensity * smooth;
+
+            g.transform.position = new Vector3(x + originalPos.x, y + originalPos.y, originalPos.z);
+
+
+            yield return null;
+        }
+
+        g.transform.position = originalPos;
+        Destroy(g);
     }
 
 
---
+++ Assets/Scripts/TestCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCube : MonoBehaviour {
+
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            TextSpawnerManager.Instance.SpawnSoundEffect("\"ニャ!\"", Color.white,transform,offset,7);
+        }
+	}
+}
