7f6865b8bace3e84d12cb33625b2fc2acf7eb2c1
ivanleong <ivan_leong_97@hotmail.com>
2017-08-07 13:14:41 +0800

    WIP

--- Assets/Scripts/Spells/HoldSpell.cs
+++ Assets/Scripts/Spells/HoldSpell.cs
@@ -2,54 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HoldSpell : Spell {
+//public class HoldSpell : Spell {
 
-    protected bool hasFlag = false;
-    protected bool casted = false;
+//    protected bool hasFlag = false;
+//    protected bool casted = false;
 
-    protected override void Hold()
+//    protected override void Hold()
+//    {
+//        if (!casted)
+//        {
+//            if (!hasFlag)
+//            {
+//                spellGO = Instantiate(spellPrefab, currentInteractingController.transform);
+
+//                hasFlag = true;
+//            }
+//            else
+//            {
+//                var em = spellGO.GetComponent<ParticleSystem>().emission;
+
+//                var emsmoke = spellGO.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().emission;
+
+//                em.enabled = true;
+//                emsmoke.enabled = true;
+
+//                casted = true;
+//            }
+//        }
+//    }
+
+//    protected override void Release()
+//    {
+//        if (casted)
+//        {
+//            var em = spellGO.GetComponent<ParticleSystem>().emission;
+
+//            var emsmoke = spellGO.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().emission;
+
+//            var emindicator = indicator.GetComponent<ParticleSystem>().emission;
+
+//            emindicator.enabled = false;
+//            em.enabled = false;
+//            emsmoke.enabled = false;
+
+//            Destroy(indicator, 0.1f);
+//            Destroy(spellGO, 0.5f);
+
+//            casted = false;
+//        }
+        
+//    }
+//}
+
+public class HoldSpell : SpellHandler
+{
+    void OnHold()
     {
-        if (!casted)
-        {
-            if (!hasFlag)
-            {
-                spellGO = Instantiate(spellPrefab, currentInteractingController.transform);
-
-                hasFlag = true;
-            }
-            else
-            {
-                var em = spellGO.GetComponent<ParticleSystem>().emission;
-
-                var emsmoke = spellGO.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().emission;
-
-                em.enabled = true;
-                emsmoke.enabled = true;
-
-                casted = true;
-            }
-        }
+        Cast();
     }
 
-    protected override void Release()
+    void OnRelease()
     {
-        if (casted)
-        {
-            var em = spellGO.GetComponent<ParticleSystem>().emission;
-
-            var emsmoke = spellGO.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().emission;
-
-            var emindicator = indicator.GetComponent<ParticleSystem>().emission;
-
-            emindicator.enabled = false;
-            em.enabled = false;
-            emsmoke.enabled = false;
-
-            Destroy(indicator, 0.1f);
-            Destroy(spellGO, 0.5f);
-
-            casted = false;
-        }
-        
+        Decast();
     }
 }
