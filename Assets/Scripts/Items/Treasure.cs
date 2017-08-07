a20716224a6c768af9a3da869d97fe759a764560
jkbhk <marcusheadgotscarm16@gmail.com>
2017-08-07 17:53:29 +0800

    Attached sphereCollider to treasure

--- Assets/Scripts/Items/Treasure.cs
+++ Assets/Scripts/Items/Treasure.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[RequireComponent(typeof(SphereCollider))]
 public class Treasure : MonoBehaviour, IDamagable {
 
 
@@ -11,6 +11,7 @@ public class Treasure : MonoBehaviour, IDamagable {
     protected void Awake()
     {
         col = GetComponent<Collider>();
+        col.isTrigger = true;
     }
 
     public Collider Collider
