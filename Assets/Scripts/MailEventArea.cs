32b2d295c44ae5d59529538a39f83b49776de975
Unknown <marcusheadgotscarm16@gmail.com>
2017-08-09 21:09:53 +0800

    Optimized mail event area

--- Assets/Scripts/MailEventArea.cs
+++ Assets/Scripts/MailEventArea.cs
@@ -7,23 +7,23 @@ public class MailEventArea : MonoBehaviour {
     [SerializeField]
     private float eventRadius;
     [SerializeField]
+    private LayerMask layerM;
+    [SerializeField]
     private MessageManager.Mail mail;
     private bool isTriggered = false;
 
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        foreach(Collider col in Physics.OverlapSphere(transform.position, eventRadius))
-        {
-            if(col.gameObject.layer == LayerMask.NameToLayer("Player"))
-            {
-                MessageManager.Instance.SendMail(mail.Title, mail.Message, mail.Clip);
-                isTriggered = true;
-            }
 
-           
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Physics.OverlapSphere(transform.position, eventRadius, layerM).Length > 0)
+        {
+            MessageManager.Instance.SendMail(mail.Title, mail.Message, mail.Clip);
+            isTriggered = true;
         }
+           
 
 
 
