62a8e4c25289be8d65cbc836323d7629d06fa6fb
Samuel Ng <eclmist@yahoo.com>
2017-08-10 15:12:21 +0800

    Added CurvedUI Asset

---
+++ Assets/3rd Party/CurvedUI/Scripts/Experimental/CurvedUITMPSubmesh.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#if CURVEDUI_TMP
+using TMPro;
+#endif
+
+namespace CurvedUI
+{
+    [ExecuteInEditMode]
+    public class CurvedUITMPSubmesh : MonoBehaviour
+    {
+#if CURVEDUI_TMP
+
+      
+        VertexHelper vh;     
+        Mesh savedMesh;
+
+
+        public void UpdateSubmesh(bool tesselate, bool curve)
+        {
+            TMP_SubMeshUI TMPsub = gameObject.GetComponent<TMP_SubMeshUI>();
+            if (TMPsub == null) return;
+
+            CurvedUIVertexEffect crvdVE = gameObject.AddComponentIfMissing<CurvedUIVertexEffect>();
+
+            if (tesselate || savedMesh == null || vh == null || (!Application.isPlaying))
+            {
+                vh = new VertexHelper(TMPsub.mesh);
+                ModifyMesh(crvdVE);
+
+                savedMesh = new Mesh();
+                vh.FillMesh(savedMesh);
+                crvdVE.TesselationRequired = true;
+            }
+            else if (curve)
+            {
+                ModifyMesh(crvdVE);
+                vh.FillMesh(savedMesh);
+                crvdVE.CurvingRequired = true;
+            }
+
+            TMPsub.canvasRenderer.SetMesh(savedMesh);
+        }
+
+
+
+
+        void ModifyMesh(CurvedUIVertexEffect crvdVE)
+        {
+#if UNITY_5_1
+		    crvdVE.ModifyMesh(vh.GetUIVertexStream);
+#else
+            crvdVE.ModifyMesh(vh);
+#endif
+        }
+#endif 
+    }
+}
+
+
