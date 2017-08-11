373c3615cab2b17db021ac25fede2f4bc5881928
Unknown <dragolee@hotmail.com>
2017-08-11 10:21:04 +0800

    text spawner keeps getting fked

--- Assets/Scripts/Manager/TextSpawnerManager.cs
+++ Assets/Scripts/Manager/TextSpawnerManager.cs
@@ -19,38 +19,6 @@ public class TextSpawnerManager : MonoBehaviour
 		Instance = this;
 	}
 
-	// Generates a text that floats above the given transform and fades away
-	public void SpawnText(string text, Color color, Transform t)
-	{
-		Collider c = t.GetComponent<Collider>();
-		SpawnedText st;
-
-    public void SpawnText(string text, Color color, Transform t, float scale, bool impact = false)
-    {
-        Collider c = t.GetComponent<Collider>();
-        SpawnedText st;
-
-        if (c)
-            st = Instantiate(textPrefab, (t.position + t.up * (c.bounds.size.y) + offset), t.rotation);
-        else
-            st = Instantiate(textPrefab, (t.position + t.up * +offset.y), t.rotation);
-
-        st.transform.localScale *= scale;
-        st.SetText(text);
-        st.SetColor(color);
-
-        float tempLength = st.GetComponentInChildren<Animator>().GetCurrentAnimatorClipInfo(0).Length;
-
-        if (impact)
-        {
-            StartCoroutine(Shake(st.gameObject, tempLength, 0.5f));
-        }
-            
-
-        Destroy(st.gameObject, tempLength + 1);
-
-    }
-
 
     // Generates a text that stays at a position 
     public void SpawnSoundEffect(string text, Color color, Transform t, Vector3 offset, float duration, float scale = 1)
@@ -70,134 +38,7 @@ public class TextSpawnerManager : MonoBehaviour
     }
 
 
-    private IEnumerator Shake(GameObject g, float shakeDuration, float shakeIntensity)
-    {
-
-        float timeElapsed = 0.0f;
-
-        Vector3 originalPos = g.transform.position;
-
-        while (timeElapsed < shakeDuration)
-        {
-
-            timeElapsed += Time.deltaTime;
-
-
-            // As the shake approaches the end, the smooth value increases
-            // The smooth value will be multiplied to the shake intensity
-            // This will slowly take away the shake intensty over time so it doesn't look like it snaps
-            float percentComplete = timeElapsed / shakeDuration;
-            float smooth = 1.0f - Mathf.Clamp(percentComplete, 0.0f, 1.0f);
-
-            // Multiplying by ( *2 - 1 ) gets a range from -1 to 1
-            float x = Random.value * 2.0f - 1.0f;
-            float y = Random.value * 2.0f - 1.0f;
-
-            x *= shakeIntensity * smooth;
-            y *= shakeIntensity * smooth;
-
-            g.transform.position = new Vector3(x + originalPos.x, y + originalPos.y, originalPos.z);
-
-
-            yield return new WaitForEndOfFrame();
-        }
-
-        g.transform.position = originalPos;
-        Destroy(g);
-    }
-
-		st.SetText(text);
-		st.SetColor(color);
-		Destroy(st.gameObject, st.GetComponentInChildren<Animator>().GetCurrentAnimatorClipInfo(0).Length);
-
-	}
-
-
-	public void SpawnText(string text, Color color, Transform t, float scale, bool impact = false)
-	{
-		Collider c = t.GetComponent<Collider>();
-		SpawnedText st;
-
-		if (c)
-			st = Instantiate(textPrefab, (t.position + t.up * (c.bounds.size.y) + offset), t.rotation);
-		else
-			st = Instantiate(textPrefab, (t.position + t.up * +offset.y), t.rotation);
-
-		st.transform.localScale *= scale;
-		st.SetText(text);
-		st.SetColor(color);
-
-		float tempLength = st.GetComponentInChildren<Animator>().GetCurrentAnimatorClipInfo(0).Length;
-
-		if (impact)
-		{
-			StartCoroutine(Shake(st.gameObject, tempLength, 0.5f));
-		}
-
-
-		Destroy(st.gameObject, tempLength + 1);
-
-	}
-
-
-	// Generates a text that stays at a position 
-	public void SpawnSoundEffect(string text, Color color, Transform t, Vector3 offset, float duration, float scale = 1)
-	{
-
-
-		SpawnedText st = Instantiate(textPrefab, t.position + offset, Quaternion.identity);
-
-
-		st.transform.localScale *= scale;
-		st.SetText(text);
-		st.SetColor(color);
-		st.GetComponentInChildren<Animator>().enabled = false;
-		st.GetComponentInChildren<Outline>().enabled = true;
-		StartCoroutine(Shake(st.gameObject, duration, 0.07f));
-
-	}
-
-
-	private IEnumerator Shake(GameObject g, float shakeDuration, float shakeIntensity)
-	{
-
-		float timeElapsed = 0.0f;
-
-		Vector3 originalPos = g.transform.position;
-
-		while (timeElapsed < shakeDuration)
-		{
-
-			timeElapsed += Time.deltaTime;
-
-
-			// As the shake approaches the end, the smooth value increases
-			// The smooth value will be multiplied to the shake intensity
-			// This will slowly take away the shake intensty over time so it doesn't look like it snaps
-			float percentComplete = timeElapsed / shakeDuration;
-			float smooth = 1.0f - Mathf.Clamp(percentComplete, 0.0f, 1.0f);
-
-			// Multiplying by ( *2 - 1 ) gets a range from -1 to 1
-			float x = Random.value * 2.0f - 1.0f;
-			float y = Random.value * 2.0f - 1.0f;
-
-			x *= shakeIntensity * smooth;
-			y *= shakeIntensity * smooth;
-
-			g.transform.position = new Vector3(x + originalPos.x, y + originalPos.y, originalPos.z);
-
-
-			yield return new WaitForEndOfFrame();
-		}
-
-		g.transform.position = originalPos;
-		Destroy(g);
-	}
-
-
-
-}
-    public void SpawnText(string text, Color color, Transform t, float scale, bool impact = false)
+    public void SpawnText(string text, Color color, Transform t, float scale = 1, bool impact = false)
     {
         Collider c = t.GetComponent<Collider>();
         SpawnedText st;
@@ -224,24 +65,6 @@ public class TextSpawnerManager : MonoBehaviour
     }
 
 
-    // Generates a text that stays at a position 
-    public void SpawnSoundEffect(string text, Color color, Transform t, Vector3 offset, float duration, float scale = 1)
-    {
-
-        
-        SpawnedText st = Instantiate(textPrefab,t.position + offset,Quaternion.identity);
-        
-
-        st.transform.localScale *= scale;
-        st.SetText(text);
-        st.SetColor(color);
-        st.GetComponentInChildren<Animator>().enabled = false;
-        st.GetComponentInChildren<Outline>().enabled = true;
-        StartCoroutine(Shake(st.gameObject, duration, 0.07f));
-
-    }
-
-
     private IEnumerator Shake(GameObject g, float shakeDuration, float shakeIntensity)
     {
 
@@ -277,3 +100,4 @@ public class TextSpawnerManager : MonoBehaviour
         g.transform.position = originalPos;
         Destroy(g);
     }
+}
