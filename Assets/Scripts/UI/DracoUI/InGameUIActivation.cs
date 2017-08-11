6235d34aeb861fd7bc572972f6090b3d0c037c65
Unknown <dragolee@hotmail.com>
2017-08-11 21:43:26 +0800

    checked if device is null

--- Assets/Scripts/UI/DracoUI/InGameUIActivation.cs
+++ Assets/Scripts/UI/DracoUI/InGameUIActivation.cs
@@ -25,34 +25,36 @@ public class InGameUIActivation : MonoBehaviour
     {
         if (currentInteractingCtrl)
         {
-            if (currentInteractingCtrl.Device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
-            {
-                if (menu != null)
-                {
-					iGMA.Deactivate();
-                    menu.SetActive(false);
-                }
+			if (currentInteractingCtrl.Device != null)
+			{
+				if (currentInteractingCtrl.Device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+				{
+					if (menu != null)
+					{
+						iGMA.Deactivate();
+						menu.SetActive(false);
+					}
 
-                Vector3 localOffset = Vector3.zero;
-                localOffset += transform.right * offset.x;
-                localOffset += transform.up * offset.y;
-                localOffset += transform.forward * offset.z;
+					Vector3 localOffset = Vector3.zero;
+					localOffset += transform.right * offset.x;
+					localOffset += transform.up * offset.y;
+					localOffset += transform.forward * offset.z;
 
-                menu.SetActive(true);
-                menu.transform.position = currentInteractingCtrl.transform.position + localOffset;
-                menu.transform.rotation = Quaternion.Euler(Quaternion.identity.x, currentInteractingCtrl.Device.transform.rot.eulerAngles.y +180, Quaternion.identity.z);
-                
-            }
+					menu.SetActive(true);
+					menu.transform.position = currentInteractingCtrl.transform.position + localOffset;
+					menu.transform.rotation = Quaternion.Euler(Quaternion.identity.x, currentInteractingCtrl.Device.transform.rot.eulerAngles.y + 180, Quaternion.identity.z);
 
-            else if (currentInteractingCtrl.Device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
-            {
-                if (menu != null && currentInteractingCtrl.UI == null)
-                {
-					iGMA.Deactivate();
-					menu.SetActive(false);
-                }
-            }
+				}
 
+				else if (currentInteractingCtrl.Device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+				{
+					if (menu != null && currentInteractingCtrl.UI == null)
+					{
+						iGMA.Deactivate();
+						menu.SetActive(false);
+					}
+				}
+			}
         }
     }
 }
